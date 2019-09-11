8937335491e7664b85e83432b430aeb5f17edc2b
omer.isik <isikomer88@gmail.com>
2019-09-11 14:20:37 +0300

    DAL Çalışma

---
+++ MyBlog.DAL/Config/CategoryConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyBlog.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBlog.DAL.Config
+{
+    class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.Property(x => x.CategoryName).IsRequired().HasMaxLength(256);
+
+            builder.Property(x => x.Description).IsRequired(false);
+        }
+    }
+}
---
+++ MyBlog.DAL/Contexts/MyBlogContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyBlog.DAL.Config;
+using MyBlog.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBlog.DAL.Contexts
+{
+    public class MyBlogContext:DbContext
+    {
+        public MyBlogContext(DbContextOptions<MyBlogContext>options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+            modelBuilder.ApplyConfiguration(new PostConfiguration());
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+        }
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Post> Posts { get; set; }
+    }
+}
